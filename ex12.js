cef00a10d1cd98aaf7b96d8db3e4fa9b0410b3ee
song <songsm5504@gmail.com>
2020-01-29 11:15:31 +0900

    javaScript

--- ex12.js
+++ ex12.js
@@ -9,3 +9,33 @@ var calc1 = function(a, b) {
   return a + b;
 };
 console.log(calc1(60, 20));
+
+// 함수내부에 함수를 활용하여 재귀 (재귀호출형태의 함수)
+function calc2(func) {
+  console.log(func(40, 60));
+}
+
+calc2(calc1);
+
+// 함수를 배열로 선언하여 사용하는 경우
+let fncArr = [
+  function() {
+    console.log("a1");
+  },
+  "a2"
+];
+
+// 함수를 맵으로 선언하여 사용하는 경우
+let fncMap = new Map();
+fncMap.set("calcFnc1", function(n1, n2) {
+  return n1 * n2;
+});
+fncMap.set("calcFnc2", function(n1, n2) {
+  return n1 / n2;
+});
+
+fncArr[0]();
+
+console.log(fncArr[1]);
+console.log(fncMap.get("calcFnc1")(24, 365));
+console.log(fncMap.get("calcFnc2")(3000, 24));
